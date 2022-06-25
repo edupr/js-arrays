bcad09bfb1c7862d84dc0f83cd980f23f7fe65a8
Edu <particularspam@gmail.com>
2022-06-24 23:11:10 -0500

    Reto, verificar edades < 15

--- server/09-some.js
+++ server/09-some.js
@@ -19,5 +19,28 @@ console.log("every ", rta2);
 function solution(numbers) {
     return numbers.every(element => element % 2 === 0);
 }
-
 console.log(solution([2,4,6,9]));
+
+// Reto, validar si todas las edades son mayores a 15
+const team = [
+    {
+      name: "Nicolas",
+      age: 12,
+    },
+    {
+      name: "Andrea",
+      age: 8,
+    },
+    {
+      name: "Zulema",
+      age: 2,
+    },
+    {
+      name: "Santiago",
+      age: 14,
+    },
+  ];
+
+function solution(team) {
+    return team.every(element => element.age < 15); }
+console.log(solution(team));
