cdf9cf5421c3cdbee9e26fd32b785de14c701cb8
Edu <particularspam@gmail.com>
2022-06-25 20:57:35 -0500

    playground

--- server/12-join.js
+++ server/12-join.js
@@ -16,3 +16,22 @@ const title = "The title of the book";
 console.log(title.split(" "));
 const urlFinal = title.split(" ").join("-").toLowerCase();
 console.log(urlFinal);
+
+const title2 = "The title of the book";
+const urlFinal2 = title2.split("o").join("-").toLowerCase();
+console.log("title2: ", title2)
+console.log("url2:", urlFinal2);
+
+// Playground
+// Construye una URL con base a un string
+const solution1 = ("La forma de correr Python");
+const solution2 = solution("La API para nunca parar de aprender");
+const solution3 = solution("Curso de arrays");
+
+function solution(title) {
+    return title.split(" ").join("-").toLowerCase();
+}; 
+
+console.log(solution(solution1));
+console.log(solution(solution2));
+console.log(solution(solution3));
